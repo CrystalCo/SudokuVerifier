6ec38864a62cbe5d73ab677ef67df1a8869e343d
Crystal C <crystal.contr@gmail.com>
2018-10-24 13:01:28 -0500

    final version.  If all rows and are columns are unique, it should display Sudoku Verified, else print out row(s) and column(s) that are wrong.

--- assign5.py
+++ assign5.py
@@ -6,9 +6,6 @@ def check_sudoku(file_name):
     input_file = open(file_name)
     lines = input_file.readlines()
     input_file.close()
-    
-    # Verification content
-    verified = True
 
     # Data loaded into a 2-dimensional list
     lines_to_string = ''.join(lines).splitlines()
@@ -20,38 +17,27 @@ def check_sudoku(file_name):
         for col in range(line_length):
             solution_list[row].append(lines_to_string[row].split()[col])
 
-    verified = check_rows(solution_list, verified)
-    verified = check_col(solution_list, verified)
-
-    if verified:
-        print('Sudoku Verified.')
+    check_rows(solution_list)
+    check_col(solution_list)
+    verified(solution_list)
 
 
-def check_rows(solution_list, is_verified):
+def check_rows(solution_list):
     'Verifies that each row contains all digits 1 thru 9. If not, displays each bad column and indicates the first bad digit in the row.'
     
-    row_solution_counter = 0
     for row in solution_list:       ##
         a_set = set(row)
-        if len(row) == len(a_set):
-            # print('Sudoku Verified')
-            row_solution_counter += 1
-        else:
+        if len(row) != len(a_set):
             print('Row {} bad: {}  '.format(solution_list.index(row)+1, ' '.join(str(num) for num in row)), end='') 
             tmp_row = list(row)
             for c in a_set:
                 tmp_row.remove(c)
             print('1st Bad Value: ' + str(tmp_row[0]))
 
-    if row_solution_counter == len(solution_list):
-        is_verified = False
-    
-    return is_verified
 
-def check_col(solution_list, is_verified):
+def check_col(solution_list):
     'Verifies that each column contains all digits 1 thru 9. If not, displays each bad row and indicates the first bad digit in the column.'
 
-    col_solution_counter = 0
     num_cols = len(solution_list[0])
 
     for index in range(num_cols):
@@ -60,21 +46,35 @@ def check_col(solution_list, is_verified):
         for row in solution_list:
             a_set.add(row[index])
             a_list.append(row[index])
-        if len(solution_list) == len(a_set):
-            col_solution_counter +=1
-        else:
+        if len(solution_list) != len(a_set):
             print('Col {} bad: {}  '
                 .format(index+1, 
-                    ' '.join(str(num) for num in [solution_list[k][index] for k in range(num_cols)]), end=''))
+                    ' '.join(str(num) for num in [solution_list[k][index] for k in range(num_cols)])), end='')
             for c in a_set:
                 a_list.remove(c)
-            print('1st Bad Value: {}'.format(a_list[0]) )
-     
-    if col_solution_counter == len(solution_list[0]):
-        is_verified = False
-        return is_verified
+            print('1st Bad Value: ' + str(a_list[0]) )
 
+def verified(solution_list):
+    row_counter = 0
+    col_counter = 0
+
+    for row in solution_list:
+        a_set = set(row)
+        if len(row) == len(a_set):
+            row_counter += 1
     
+    num_cols = len(solution_list[0])
+    for index in range(num_cols):
+        a_set = set()
+        for row in solution_list:
+            a_set.add(row[index])
+        if len(solution_list) == len(a_set):
+            col_counter +=1
+
+    if (row_counter == num_cols) and (col_counter == num_cols):
+        print('Sudoku Verified.')
+
+
 user_filename = input('Enter filename: ')
 check_sudoku(user_filename)
 
